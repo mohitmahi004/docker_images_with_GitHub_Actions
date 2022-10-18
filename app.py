5a6dcffd488582567a0c2ecbe5a83c358815dc89
mohitmahi004 <58883261+mohitmahi004@users.noreply.github.com>
2022-10-19 01:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-        return "Hello, world!"
+        return "Hello, world from Mohit"
 
 if __name__ == '__main__':
     app.run(host ="0.0.0.0", port=8000)
